c97e9270d1fed776d82b91ead9cd84e1e898fe04
dsalex1 <dsalex1@ymail.com>
2019-02-25 02:39:14 +0100

    several fixes

--- src/lightHouseSimulator/Example.java
+++ src/lightHouseSimulator/Example.java
@@ -1,5 +1,6 @@
 package lightHouseSimulator;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
@@ -7,9 +8,19 @@ import javax.imageio.ImageIO;
 public class Example {
 
     public static void main(String[] args) throws IOException {
-        LightHouseSimulator m = new LightHouseSimulator();
-        m.setMappingMode();
-        m.setGapsRatio(1, 0);
-        m.setData(ImageIO.read(new File(Example.class.getResource("/lightHouseSimulator/test.png").getFile())));
+        // instanceiate the simulator
+        LightHouseSimulator sim = new LightHouseSimulator();
+
+        // set mode to mapping(down sampling the image data)
+        sim.setMappingMode();
+
+        // make all pixels render, pretending the gaps where not there
+        sim.setGapsRatio(1, 0);
+
+        // loading an image from file
+        BufferedImage imgData = ImageIO.read(new File(Example.class.getResource("test.png").getFile()));
+
+        // display this data on the lighthouse
+        sim.setData(imgData);
     }
 }
